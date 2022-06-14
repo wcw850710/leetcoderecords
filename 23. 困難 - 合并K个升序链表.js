38a8a7589480f826779252b677cfaa129ef7021b
575 <61653790+wcw850710@users.noreply.github.com>
2022-06-14 10:24:18 +0800

    Create 23. 困難 - 合并K个升序链表.js

---
+++ 23. 困難 - 合并K个升序链表.js
@@ -0,0 +1,53 @@
+///https://leetcode.cn/problems/merge-k-sorted-lists/submissions/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    var max = 10001
+    var dummy = new ListNode(-10001)
+    var p = dummy
+    var obj = {}
+
+    for (var i = 0; i < lists.length; i++) {
+        var e = lists[i]
+        if (e != null) {
+            if (obj[e.val] == null) obj[e.val] = [e]
+            else obj[e.val].push(e)
+        }
+    }
+
+    while(Object.keys(obj).length > 0) {
+        var min = max
+        for (var valStr in obj) {
+            var val = Number(valStr)
+            if (val <= min) min = val
+        }
+        var newObj = {}
+        for (var i = 0; i < obj[min].length; i++) {
+            var e = obj[min][i]
+            p.next = e
+            p = p.next
+            if (e.next != null) {
+                if (obj[e.next.val] != null) obj[e.next.val].push(e.next)
+                else if (newObj[e.next.val] == null) newObj[e.next.val] = [e.next]
+                else newObj[e.next.val].push(e.next)
+            }
+        }
+        delete obj[min]
+        if (Object.keys(newObj).length > 0) {
+            for (var val in newObj) {
+                obj[val] = newObj[val]
+            }
+        }
+    }
+
+    return dummy.next
+};
